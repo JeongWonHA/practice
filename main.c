d6040a58e9fb7bfefdf7d727e03331d00946b2d6
JeongWonHA <61004941+JeongWonHA@users.noreply.github.com>
2020-05-22 16:32:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 int main(){
-
+  srand(time(NULL));
+  char food[10][] = {"햄버거","치킨","짜장면","고기","국밥"};
+  printf("오늘은 뭐 먹을까?");
+  scanf("Enter을 누르면 됩니다!\n");
+  
+  int length = sizeof(food)/sizeof(food[0]);
+  int a = rand%lenght;
+  printf("%s",food[a]);
+  
   return 0;
 }
